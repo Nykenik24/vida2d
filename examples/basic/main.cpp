bec2efd970d84deb27fc8d79a37bf7e94448690e
Nykenik24 <Nykenik24@proton.me>
2026-06-06 23:01:44 +0200

    examples: update basic

--- examples/basic/main.cpp
+++ examples/basic/main.cpp
@@ -1,4 +1,5 @@
 #include "vida/core/Color.hpp"
+#include "vida/renderer3d/Camera.hpp"
 #include "vida/renderer3d/Renderer3D.hpp"
 #include "vida/window/Window.hpp"
 
@@ -6,13 +7,14 @@ int main() {
   Vida::Window window(800, 600, "Vida 2D");
   Vida::Renderer3D renderer(window);
 
-  renderer.GetCamera().Move({2, 0, 5});
-  renderer.GetCamera().Point({1, 0, 0});
+  Vida::Camera &cam = renderer.GetCamera();
+  cam.Move({2, 0, 5});
+  cam.Point({1, 0, 0});
 
   while (!window.ShouldClose()) {
     window.PollEvents();
     renderer.Clear(Vida::ColorRGBA::Black);
-    renderer.DrawCube({0, 0, 0}, {1, 1, 1}, Vida::ColorRGBA::Blue);
+    renderer.DrawCube({0, 0, 0}, {1, 1, 1}, Vida::ColorRGBA::White);
     renderer.Present();
   }
 }
